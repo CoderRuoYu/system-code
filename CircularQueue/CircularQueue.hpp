c915a989513f13047785eb5347b9211faefe2148
Coder RuoYu <ruoyubushiyu@gmail.com>
2026-03-06 12:26:04 +0800

    使用环形队列实现的单生产者单消费者模型

--- CircularQueue/CircularQueue.hpp
+++ CircularQueue/CircularQueue.hpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <semaphore.h>
 
-
 using std::cout;
 using std::endl;
 #define DefaultQueueSize 8
@@ -11,16 +10,42 @@ class CircularQueue
 {
 public:
     CircularQueue()
+        : _v(DefaultQueueSize),
+          _capacity(DefaultQueueSize),
+          _consumerStep(0),
+          _producerStep(0)
     {
-
-
+        sem_init(&_dataSpace, 0, DefaultQueueSize);
+        sem_init(&_dataNums, 0, 0);
+    }
+    void ProductGoods(int data)
+    {
+        sem_wait(&_dataSpace);
+        _v[_producerStep] = data;
+        _producerStep++;
+        _producerStep %= _capacity;
+        sem_post(&_dataNums);
+    }
+    int ConsumeGoods()
+    {
+        sem_wait(&_dataNums);
+        int data = _v[_consumerStep];
+        _consumerStep++;
+        _consumerStep %= _capacity;
+        sem_post(&_dataSpace);
+        return data;
     }
     ~CircularQueue()
     {
-
+        sem_destroy(&_dataSpace);
+        sem_destroy(&_dataNums);
     }
 
 private:
     std::vector<int> _v;
-
+    int _capacity;
+    int _consumerStep;
+    int _producerStep;
+    sem_t _dataSpace;
+    sem_t _dataNums;
 };
--- CircularQueue/main.cc
+++ CircularQueue/main.cc
@@ -1,7 +1,46 @@
 #include "CircularQueue.hpp"
+#include <unistd.h>
+#include <pthread.h>
+void *Producer(void *arg)
+{
+    CircularQueue *cq = (CircularQueue *)arg;
+
+    int data = 1;
+    while (true)
+    {
+        cq->ProductGoods(data);
+        cout << "Producer produce: " << data << endl;
+        data++;
+
+        sleep(1);
+    }
+}
+
+void *Consumer(void *arg)
+{
+    CircularQueue *cq = (CircularQueue *)arg;
+
+    while (true)
+    {
+        int data = cq->ConsumeGoods();
+        cout << "Consumer consume: " << data << endl;
+
+        sleep(2);
+    }
+}
 
 int main()
 {
+    CircularQueue cq;
+
+    pthread_t producer;
+    pthread_t consumer;
+
+    pthread_create(&producer, nullptr, Producer, &cq);
+    pthread_create(&consumer, nullptr, Consumer, &cq);
+
+    pthread_join(producer, nullptr);
+    pthread_join(consumer, nullptr);
 
     return 0;
 }
